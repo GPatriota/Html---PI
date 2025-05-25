6afb0c84198c8b7ea5a047ede1f9c4e8f482b0bc
GPatriota <gpspatriota@gmail.com>
2025-05-25 15:06:24 -0300

    fix: style of about us page to emergency

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
+  imports: [CommonModule], // Import obrigatório para *ngIf funcionar
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  imports: []
 })
-export class AboutComponent {}
+export class AboutComponent {
+  sections = {
+    sobre: true,
+    historia: true,
+    missao: true,
+    visao: true,
+    valores: true,
+  };
+
+  toggleSection(section: keyof typeof this.sections) {
+    this.sections[section] = !this.sections[section];
+  }
+}
+
