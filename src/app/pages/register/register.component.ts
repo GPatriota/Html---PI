dd5fadb042955a07a4da45a86f53d6d3d32af8db
Lins <gabriellinsrosa@gmail.com>
2025-05-27 15:38:27 -0300

    Validacoes no cadastro feitas

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -26,8 +26,48 @@ export class RegisterComponent {
   ) {}
 
   onSubmit() {
+
+    if (this.username.length < 3){
+      this.error = ('O nome deve conter pelo menos 3 caracteres');
+      return;
+    }
+
+    const emailParts = this.email.split('@');
+
+    if (emailParts.length !== 2) {
+      this.error = ('E-mail inválido');
+      return;
+    }
+
+    const antesAt = emailParts[0];
+    const depoisAt = emailParts[1].split('.')[0];
+
+    if (antesAt.length < 7 || depoisAt.length < 5) {
+      this.error = ('O e-mail deve ter 7 letras antes do @ e 5 depois');
+      return;
+    }
+
+    this.error = '';
+
+    const caracteresesp = /^\d+$/;
+
+    if (this.cpf.length < 11 || !caracteresesp.test(this.cpf) ){
+      this.error = ('O CPF deve conter apenas números e ter pelo menos 11 dígitos');
+      return;
+    }
+
+    if (!this.dob) {
+    this.error = 'A data de nascimento é obrigatória';
+    return;
+    }
+
+    if (this.password.length < 5){
+      this.error = ('A senha deve conter pelo menos 5 caracteres')
+      return;
+    }
+
     if (this.password !== this.confirmPassword) {
-      this.error = 'Passwords do not match';
+      this.error = 'As senhas devem ser iguais';
       return;
     }
     
